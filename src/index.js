0a4cc51cc9ec8475bda5c8d9c6081d617c6fb0a4
DokerMW <abddoker@gmail.com>
2024-04-25 21:17:17 +0500

    add: complete lesson 21 + hardtask

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import validation from './modules/validation'
 
-timer('25 april 2024');
+timer('16 may 2024');
 menu('active-menu');
 modal();
+validation();
