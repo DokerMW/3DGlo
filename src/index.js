99560acc77ec89becfb480391843daf23512cf8a
DokerMW <abddoker@gmail.com>
2024-04-28 01:07:22 +0500

    add: lesson 23

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
-timer('25 april 2024');
+timer('06 may 2024');
 menu('active-menu');
 modal();
 tabs();
+slider();
