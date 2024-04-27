f73dc37b80ed2e09cf8dce793210061a4790cb48
DokerMW <abddoker@gmail.com>
2024-04-27 15:12:51 +0500

    add: delegation menu.js

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,42 +1,38 @@
 const menu = (activeClass)=>{
-	const menuBtn = document.querySelector('.menu');
 	const menu = document.querySelector('menu');
-	const closeBtn = menu.querySelector('.close-btn');
-	const menuItems = menu.querySelectorAll('ul>li>a');
-	const linkDown = document.querySelector('a[href="#service-block"]');
 
 	const handleMenu = ()=>{
 		menu.classList.toggle(activeClass);
 	}
 
-	const smoothMenu = () =>{
-		menuItems.forEach(e=>{
-			e.addEventListener('click', (event)=>{
-				smoothScroll(e)
-			})
-		})
-	}
-	const linkSmooth = () => {
-		linkDown.addEventListener('click', (event)=>{
-			smoothScroll(linkDown)
-		})
-	}
 	const smoothScroll = (elem) =>{
-			event.preventDefault();
-			const blockID = elem.getAttribute('href').substr(1)
+		event.preventDefault();
+		const blockID = elem.getAttribute('href').substr(1)
 
-			document.getElementById(blockID).scrollIntoView({
-				behavior: 'smooth',
-				block: 'start'
-			})
-	}
-	
-	linkSmooth()
-	smoothMenu()
-	menuBtn.addEventListener('click', handleMenu);
-	closeBtn.addEventListener('click', handleMenu);
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+}
 
-	menuItems.forEach(e => e.addEventListener('click', handleMenu));
+	document.addEventListener('click', e => {
+		if(e.target.closest('.menu') || !e.target.closest('menu') && menu.classList.contains(activeClass)){
+			handleMenu();
+		}else if(e.target.closest('main>a[href="#service-block"]')){
+			let linkDown = e.target.closest('main>a[href="#service-block"]')
+			smoothScroll(linkDown)
+		}
+	})
+
+	menu.addEventListener('click', e => {
+		if(e.target.matches('menu>ul>li>a')){
+				smoothScroll(e.target)
+				handleMenu();
+		} else if(e.target.classList.contains('close-btn')){
+			handleMenu();
+		}
+	})
+
 }
 
 export default menu
